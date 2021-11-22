ca593ae3ebe0956f472e289ed59d5b83f261f9da
ysur67 <rus.muratow@gmail.com>
2021-11-22 11:58:41 +0300

    dump buffer in file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <string>
 #include <iostream>
-using std::string;
-using std::cout;
+#include <vector>
+#include <fstream>
+using namespace std;
+
+void dump_ppm(vector<int>* buffer, string filename, const int WIDTH, const int HEIGHT) {
+    ofstream image;
+    cout << "dumping buffer in " << filename << endl;
+    image.open(filename);
+    {
+		image << "P3" << endl;
+		image << WIDTH << " " << HEIGHT << endl;
+		image << "255" << endl;
+	}
+    for (auto value : *buffer) {
+        image << value << " " << endl;
+    }
+    image.close();
+    cout << "buffer dumped in " << filename << endl;
+}
 
 int main() {
-    cout << "hello" << std::endl;
+    const int WIDTH = 500;
+    const int HEIGHT = 500;
+    vector<int> buffer;
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            buffer.push_back(255);
+        }
+    }
+    dump_ppm(&buffer, "test.ppm", WIDTH, HEIGHT);
     return 0;
 }
